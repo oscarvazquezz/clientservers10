a3b4c34bb6f7877893d3ffd862ba0c3d14b3ab98
Silica <191272@ids.upchiapas.edu.mx>
2020-06-08 00:42:45 -0500

    add secret

--- CS/settings.py
+++ CS/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0_57k=0e!snl$!$vdjs-gc69oeqx((_2ffj6(!5x*!-jxt*^bp'
 
+SECRET_KEY ='SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
