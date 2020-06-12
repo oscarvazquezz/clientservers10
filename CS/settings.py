2e7c6da5b274363bdc869b71c8da46d785a19de2
Silica <191272@ids.upchiapas.edu.mx>
2020-06-11 23:10:54 -0500

    agregar rest swagger

--- CS/settings.py
+++ CS/settings.py
@@ -47,7 +47,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',),
     'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework.authentication.TokenAuthentication',),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
     'PAGE_SIZE': 100
 }
 
