70be7f43d937232dacee73b277e876bf21f221ad
Silica <191272@ids.upchiapas.edu.mx>
2020-06-14 12:11:07 -0500

    agregar vista diseño

--- CS/settings.py
+++ CS/settings.py
@@ -131,7 +131,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
