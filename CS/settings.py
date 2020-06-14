1577e6bfed81500b22b4e684fa7870d254261531
Silica <191272@ids.upchiapas.edu.mx>
2020-06-14 11:55:04 -0500

    agregar vista diseño

--- CS/settings.py
+++ CS/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'CS.urls'
@@ -127,8 +128,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 try:
     from CS.local_settings import *
--- CS/urls.py
+++ CS/urls.py
@@ -38,6 +38,6 @@ schema_view = get_swagger_view(title='Pastebin API')
 urlpatterns = [
     path('admin/', admin.site.urls),
     url('Api/',schema_view),
-    re_path(r'^',include(router.urls))
+    re_path(r'^',include(router.urls)),
     
 ]
